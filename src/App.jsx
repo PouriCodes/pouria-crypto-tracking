138a43d18d7becebfa7286ed4f46216a085de048
Pouria <p.nazirian@gmail.com>
2024-01-04 19:00:13 +0330

    routes implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import FooterComponent from './components/ui/Footer.components';
 import NavbarComponent from './components/ui/Navbar.component';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home.page';
+import FAQPage from './pages/FAQ.page';
+import AboutUsPage from './pages/AboutUs.page';
+import ContactUsPage from './pages/ContactUs.page';
 
 function App() {
   return (
     <div>
       <NavbarComponent />
       <div className="wrapper">
-        <h2>wrapper</h2>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/contact-us" element={<ContactUsPage />} />
+        </Routes>
       </div>
       <FooterComponent />
     </div>
---
+++ src/pages/ContactUs.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <h3>ContactUsPage</h3>
+    </div>
+  );
+};
+
+export default ContactUsPage;
