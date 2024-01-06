0ecef1f5faddc93a1752d61b109ed899170e9592
Pouria <p.nazirian@gmail.com>
2024-01-06 18:40:07 +0330

    FAQ about us and contact us pages implemented

--- src/components/ui/Navbar.component.jsx
+++ src/components/ui/Navbar.component.jsx
@@ -1,54 +1,60 @@
 import React, { useRef } from 'react';
+import './Navbar.css';
+
 import { Link } from 'react-router-dom';
 import { FaCoins } from 'react-icons/fa';
-import './Navbar.css';
 
 const NavbarComponent = () => {
   const btnToggleRef = useRef();
 
   const toggleMenu = () => {
-    btnToggleRef.current.click();
+    if (window.innerWidth < 992) {
+      btnToggleRef.current.click();
+    }
   };
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <FaCoins className="navbar-logo-icon" />
-        <span className="navbar-logo-text">Crypto App Tracking</span>
-      </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        ref={btnToggleRef}>
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item" onClick={toggleMenu}>
-            <Link className="nav-link" to="/">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item" onClick={toggleMenu}>
-            <Link className="nav-link" to="/faq">
-              FAQ
-            </Link>
-          </li>
-          <li className="nav-item" onClick={toggleMenu}>
-            <Link className="nav-link" to="/about-us">
-              About Us
-            </Link>
-          </li>
-          <li className="nav-item" onClick={toggleMenu}>
-            <Link className="nav-link" to="/contact-us">
-              Contact Us
-            </Link>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <div>
+          <FaCoins className="navbar-logo-icon" />
+          <span className="navbar-logo-text">Crypto Tracking</span>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          ref={btnToggleRef}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item" onClick={toggleMenu}>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item" onClick={toggleMenu}>
+              <Link className="nav-link" to="/faq">
+                FAQ
+              </Link>
+            </li>
+            <li className="nav-item" onClick={toggleMenu}>
+              <Link className="nav-link" to="/about-us">
+                About Us
+              </Link>
+            </li>
+            <li className="nav-item" onClick={toggleMenu}>
+              <Link className="nav-link" to="/contact-us">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- src/pages/AboutUs.page.jsx
+++ src/pages/AboutUs.page.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import aboutImg from '../assets/images/about.png';
 
 const AboutUsPage = () => {
   return (
-    <div>
-      <h5>AboutUsPage</h5>
-      <h5>AboutUsPage</h5>
-      <h5>AboutUsPage</h5>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 bg-info p-3 rounded">
+          <h3>About Us</h3>
+        </div>
+      </div>
+      <div className="row align-items-center">
+        <div className="col-md-7 mt-3">
+          Ex velit minim aute laboris in exercitation. Consequat est incididunt
+          incididunt minim commodo esse fugiat sit dolor dolore amet. Id labore
+          et quis cupidatat sint et voluptate magna nulla amet. Nostrud minim ea
+          id dolor consectetur minim pariatur adipisicing reprehenderit dolore.
+          Ex velit minim aute laboris in exercitation. Consequat est incididunt
+          incididunt minim commodo esse fugiat sit dolor dolore amet. Id labore
+          et quis cupidatat sint et voluptate magna nulla amet. Nostrud minim ea
+          id dolor consectetur minim pariatur adipisicing reprehenderit dolore.
+        </div>
+        <div className="col-md-5 text-center">
+          <img src={aboutImg} alt="about" className="img-fluid about-img" />
+        </div>
+      </div>
     </div>
   );
 };
